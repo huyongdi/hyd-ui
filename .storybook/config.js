346b41713e7d1eedc12c9dac003b2df14b28b1bb
HYD <354224080@qq.com>
2019-06-01 09:37:06 +0800

    '修改book的左侧标题'

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,17 @@
-import { configure } from '@storybook/react';
 
+import { configure, addDecorator } from '@storybook/react';
+import { withOptions } from '@storybook/addon-options';
+import { name, repository, version } from "../package.json"
 // automatically import all files ending in *.stories.js
 const req = require.context('../stories', true, /\.stories\.js$/);
 function loadStories() {
   req.keys().forEach(filename => req(filename));
 }
+console.log(withOptions);
 
+addDecorator(withOptions({
+  name: `${name} v${version}`,
+  url: repository,
+  sidebarAnimations: true,
+}))
 configure(loadStories, module);
